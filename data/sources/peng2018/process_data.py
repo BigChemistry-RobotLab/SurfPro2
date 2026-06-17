341c428b137b4505ceb61107963e84ca5ba85e28
William E. Robinson <werobinson6@gmail.com>
2026-06-17 16:44:39 +0200

    update data citation graph

---
+++ data/sources/peng2018/process_data.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+SOURCE_FILE = "source_data/peng2018_table_1.csv"
+PROCESSED_FILE = "processed_data/peng2018.csv"
+
+df = pd.read_csv(SOURCE_FILE)
+
+df["Gamma_max"] = df["Γmaxd (*10−5 mol/m2)"] / 10**5
+df["CMC"] = df["cmc (mmol/L)"] / 1000
+
+replace_columns = {
+    "Name": "identifier",
+    "Amine (nm2)": "Area_min",
+    "γmcc (mN/m)": "AW_ST_CMC",
+}
+
+drop_columns = [
+    "cmc (mmol/L)",
+    "Γmaxd (*10−5 mol/m2)",
+]
+
+df = df.rename(columns=replace_columns)
+
+df = df.drop(columns=drop_columns)
+
+if "CMC" in df.columns:
+    df["pCMC"] = -np.log10(df.CMC)
+elif "pCMC" in df.columns:
+    df["CMC"] = 10**-df.pCMC
+
+if "pC20" in df.columns:
+    df["C20"] = 10**-df.pC20
+elif "C20" in df.columns:
+    df["pC20"] = -np.log10(df.C20)
+
+smiles_list = []
+inchi_list = []
+mol_wts = []
+for i, row in df.iterrows():
+    smiles = row.SMILES
+
+    if pd.isna(smiles):
+        mol = None
+        smiles = ""
+        inchi = ""
+        mw = np.nan
+    else:
+        mol = Chem.MolFromSmiles(smiles)
+        smiles = Chem.MolToSmiles(mol)
+        inchi = Chem.MolToInchi(mol)
+        mw = Descriptors.MolWt(mol)
+
+    smiles_list.append(smiles)
+    inchi_list.append(inchi)
+    mol_wts.append(mw)
+
+df["SMILES"] = smiles_list
+df["Molecular_Weight"] = mol_wts
+df["InChI"] = inchi_list
+
+df = df[df.SMILES != ""]
+
+df.to_csv(PROCESSED_FILE, index=False)
