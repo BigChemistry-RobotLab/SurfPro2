8fba6f544ef2f7a22b6595750ce453202b720e48
William E. Robinson <werobinson6@gmail.com>
2026-07-03 13:46:26 +0200

    update plots for publication

--- scripts/database_characteristics.py
+++ scripts/database_characteristics.py
@@ -6,7 +6,6 @@ import toml
 import sqlite3
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -34,23 +33,36 @@ SELECT
 FROM measurements m
 LEFT JOIN compounds c ON c.compound_id = m.compound_id
 LEFT JOIN units u on u.unit_id = m.unit_id
-WHERE m.property_type_id = :prop_id;
+LEFT JOIN measurement_flags m_flag USING(measurement_id)
+WHERE m.property_type_id = :prop_id AND m_flag.data_flag_id IS NULL; -- remove flagged data
 """
 log_convert_props = ["C20", "CMC", "Area_min", "Gamma_max"]
 
-fig, ax = plt.subplots(ncols=df_prop.shape[0])
+fig, ax = plt.subplots(
+    figsize=(17.1 / 2.54, 10 / 2.54),
+    nrows=2,
+    ncols=df_prop.shape[0] // 2,
+)
+
+ax = ax.flatten()
+labels = ["a", "b", "c", "d", "e", "f"]
 for i, row in df_prop.iterrows():
     df = pd.read_sql(measurement_query, conn, params={"prop_id": row.property_type_id})
     unit = df.unit_latex.iloc[0]
     if row.property in log_convert_props:
-        ax_title = (
-            "$-\\log_{10}(\\frac{" + row.latex_math_text + "}{" + unit + "})$"
-        )
-        ax[i].hist(-np.log10(df.value))
+        ax_title = "$\\log_{10}(\\frac{" + row.latex_math_text + "}{" + unit + "})$"
+        ax[i].hist(np.log10(df.value),color="b")
+
     else:
         ax_title = f"${row.latex_math_text}/ {unit}$"
-        ax[i].hist(df.value)
+        ax[i].hist(df.value,color="b")
 
-    ax[i].set_title(ax_title)
+    ax[i].set_xlabel(ax_title, fontsize=12)
+    ax[i].annotate(labels[i], xy)
 
+for a in ax:
+    a.tick_params(axis="both", which="major", labelsize=10)
+
+plt.tight_layout()
+plt.savefig("target/database_distributions.png", dpi=300)
 plt.show()
