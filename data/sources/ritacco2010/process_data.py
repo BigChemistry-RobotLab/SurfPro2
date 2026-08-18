e79991a545863074f69f37d32c2c33ba6486b078
William E. Robinson <werobinson6@gmail.com>
2026-08-18 16:06:03 +0200

    add new data: ritacco2010

---
+++ data/sources/ritacco2010/process_data.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+SOURCE_FILE = "source_data/ritacco2010_table_1.csv"  # Multiple source files can be provided and merged during processing.
+PROCESSED_FILE = "processed_data/ritacco2010.csv"
+
+# Load in data
+df = pd.read_csv(SOURCE_FILE)
+
+# Perform any operations required immediately below this comment (e.g. unit conversions, merging, etc.)
+df["CMC"] = df["CAC (mol/m3)"] / 1000
+df["Temp_Celsius"] = df["temperature/ K"] - 273.15
+
+replace_columns = {
+    "Trisiloxane": "identifier",
+}
+
+drop_columns = [
+    "CAC (mol/m3)",
+    "temperature/ K",
+]
+
+# The remainder of this script can remain untouched.
+
+df = df.rename(columns=replace_columns)
+
+df = df.drop(columns=drop_columns)
+
+if "CMC" in df.columns:
+    df["pCMC"] = -np.log10(df.CMC)
+elif "pCMC" in df.columns:
+    df["CMC"] = 10**-df.pCMC
+
+if "pC20" in df.columns:
+    df["C20"] = 10**-df.pC20
+elif "C20" in df.columns:
+    df["pC20"] = -np.log10(df.C20)
+
+smiles_list = []
+inchi_list = []
+mol_wts = []
+for i, row in df.iterrows():
+    smiles = row.SMILES
+
+    if pd.isna(smiles):
+        mol = None
+        smiles = ""
+        inchi = ""
+        mw = np.nan
+    else:
+        mol = Chem.MolFromSmiles(smiles)
+        smiles = Chem.MolToSmiles(mol)
+        inchi = Chem.MolToInchi(mol)
+        mw = Descriptors.MolWt(mol)
+
+    smiles_list.append(smiles)
+    inchi_list.append(inchi)
+    mol_wts.append(mw)
+
+df["SMILES"] = smiles_list
+df["Molecular_Weight"] = mol_wts
+df["InChI"] = inchi_list
+
+df = df[df.SMILES != ""]
+
+df.to_csv(PROCESSED_FILE, index=False)
