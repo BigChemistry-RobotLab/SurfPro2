cdb0227aa971c1251f21a1518dac9005b4091a97
William E. Robinson <werobinson6@gmail.com>
2026-08-26 14:06:34 +0200

    update SQL query in script

--- scripts/database_characteristics.py
+++ scripts/database_characteristics.py
@@ -28,12 +28,24 @@ measurement_query = """
 SELECT
     c.compound_id,
     c.SMILES,
+    c.Surfactant_Type,
     m.value,
-    u.latex_math_text as unit_latex
+    meth.name AS method,
+    u.latex_math_text AS unit_latex
 FROM measurements m
-LEFT JOIN compounds c ON c.compound_id = m.compound_id
-LEFT JOIN units u on u.unit_id = m.unit_id
-WHERE m.property_type_id = :prop_id;
+LEFT JOIN compounds c
+    ON c.compound_id = m.compound_id
+LEFT JOIN units u
+    ON u.unit_id = m.unit_id
+LEFT JOIN measurement_flags mf
+    USING(measurement_id)
+LEFT JOIN methods meth
+    USING(method_id)
+LEFT JOIN citations cit USING(citation_id)
+WHERE
+    m.property_type_id = :prop_id
+    AND cit.cited_id IS NULL
+    AND mf.data_flag_id IS NULL;
 """
 log_convert_props = ["C20", "CMC", "Area_min", "Gamma_max"]
 
